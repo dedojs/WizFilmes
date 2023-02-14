6bda1940fe852e167447f3c0d51c49952c45db10
dedojs <xandresousax@gmail.com>
2023-02-14 19:54:34 -0300

    refactory return get director

--- WizFilmes/WizFilmes.Infra/Data/Dtos/DirectorDtos/DiretorDto.cs
+++ WizFilmes/WizFilmes.Infra/Data/Dtos/DirectorDtos/DiretorDto.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using WizFilmes.Domain.Models;
+using WizFilmes.Infra.Data.Dtos.FilmDtos;
 
 namespace WizFilmes.Infra.Data.Dtos.DirectorDtos
 {
@@ -11,6 +12,6 @@ namespace WizFilmes.Infra.Data.Dtos.DirectorDtos
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        public IEnumerable<String> Films { get; set; }
+        public IEnumerable<FilmDtoDataReturn> Films { get; set; }
     }
 }
